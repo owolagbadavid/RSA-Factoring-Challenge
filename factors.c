6127b856d28efd6682d451e6709436f36b335b0c
David <owolagbadavid@gmail.com>
2023-09-30 20:50:13 +0100

    refactor

---
+++ factors.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#define _GNU_SOURCE
+
+
+int factorize(char *buffer);
+
+
+/**
+ * main - main function
+ *
+ *
+ * Return: void
+ */
+int main(int argc, char *argv[])
+{
+	FILE *fptr;
+	size_t count;
+	ssize_t line;
+	char *buffer = NULL;
+
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: factor <filename>\n");
+		exit(EXIT_FAILURE);
+	}
+	fptr = fopen(argv[1], "r");
+	if (fptr == NULL)
+	{
+		fprintf(stderr, "Error: can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while((line = getline(&buffer, &count, fptr)) != -1)
+	{
+		factorize(buffer);
+	}
+return (0);
+}
+
+int factorize(char *buffer)
+{
+
+	u_int32_t num;
+	u_int32_t i;
+
+	num = atoi(buffer);
+
+
+	for (i = 2; i < num; i++)
+	{
+		if (num % i == 0)
+		{
+			printf("%d=%d*%d\n",num,num/i,i);
+			break;
+		}
+	}
+
+return (0);
+}
