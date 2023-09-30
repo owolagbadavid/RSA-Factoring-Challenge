7b773e6a52dd5e04477ee9117d6835406058ff2e
David <owolagbadavid@gmail.com>
2023-09-30 21:15:00 +0100

    refactor

--- factors.c
+++ factors.c
@@ -1,62 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <stdint.h>
-#define _GNU_SOURCE
-
+#include <inttypes.h> // Include this header for PRIu64 macro
+#include <unistd.h>
 
 int factorize(char *buffer);
 
+int main(int argc, char *argv[]) {
+    FILE *fptr;
+    size_t count;
+    ssize_t line;
+    char *buffer = NULL;
 
-/**
- * main - main function
- *
- *
- * Return: void
- */
-int main(int argc, char *argv[])
-{
-	FILE *fptr;
-	size_t count;
-	ssize_t line;
-	char *buffer = NULL;
+    if (argc != 2) {
+        fprintf(stderr, "Usage: factor <filename>\n");
+        exit(EXIT_FAILURE);
+    }
+    fptr = fopen(argv[1], "r");
+    if (fptr == NULL) {
+        fprintf(stderr, "Error: can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+    while ((line = getline(&buffer, &count, fptr)) != -1) {
+        factorize(buffer);
+    }
 
-
-	if (argc != 2)
-	{
-		fprintf(stderr, "Usage: factor <filename>\n");
-		exit(EXIT_FAILURE);
-	}
-	fptr = fopen(argv[1], "r");
-	if (fptr == NULL)
-	{
-		fprintf(stderr, "Error: can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	while((line = getline(&buffer, &count, fptr)) != -1)
-	{
-		factorize(buffer);
-	}
-return (0);
+    return 0;
 }
 
-int factorize(char *buffer)
-{
+int factorize(char *buffer) {
+    uint64_t num;
 
-	u_int32_t num;
-	u_int32_t i;
+    sscanf(buffer, "%" SCNu64, &num);  // Read the number as a 64-bit unsigned integer
 
-	num = atoi(buffer);
+    for (uint64_t i = 2; i <= num; i++) {
+        if (num % i == 0) {
+            printf("%" PRIu64 "=%" PRIu64 "*%" PRIu64 "\n", num, num / i, i);
+            break;
+        }
+    }
 
-
-	for (i = 2; i < num; i++)
-	{
-		if (num % i == 0)
-		{
-			printf("%d=%d*%d\n",num,num/i,i);
-			break;
-		}
-	}
-
-return (0);
+    return 0;
 }
+
